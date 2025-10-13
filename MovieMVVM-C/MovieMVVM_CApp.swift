faf49b02f93a9ccc2117f84c3fa2162076837cac
Sirin Demir <sirin.demir@consultant.turkcell.com.tr>
2025-10-13 13:03:28 +0300

    Initial Commit

---
+++ MovieMVVM-C/MovieMVVM_CApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMVVM_CApp.swift
+//  MovieMVVM-C
+//
+//  Created by Şirin on 13.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieMVVM_CApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
